89fe1bf7a02e836345fc1dc54faf59bc3bd4ea88
shivam <shiv2299@gmail.com>
2020-03-09 20:09:19 +0530

    departments designed

--- lib/Departments.dart
+++ lib/Departments.dart
@@ -11,23 +11,37 @@ class Departments extends StatefulWidget {
 }
 
 class _DepartmentsState extends State<Departments> {
-  List<String> depts = ["CP / IT", "CE", "ME / PE", "EL", "EE"];
+  List<String> depts = [
+    "Cyber-Clan",
+    "Road to Urbanization",
+    "Pulleying the Future",
+    "Wire-More to Wireless",
+    "Lighting Ears Ahead"
+  ];
+  final icons = [
+    "assets/images/Cpit2.png",
+    "assets/images/Civil2.png",
+    "assets/images/mech2.png",
+    "assets/images/Ecel2.png",
+    "assets/images/ee2.png"
+  ];
   List<EventDetail> eventList;
+  List<Widget> children = [];
   void go(index) {
-    switch (depts[index]) {
-      case "CP / IT":
+    switch (index) {
+      case "Cyber-Clan":
         eventList = widget.event.tech.cpit;
         break;
-      case "CE":
+      case "Road to Urbanization":
         eventList = widget.event.tech.ce;
         break;
-      case "ME / PE":
+      case "Pulleying the Future":
         eventList = widget.event.tech.mepr;
         break;
-      case "EL":
+      case "Wire-More to Wireless":
         eventList = widget.event.tech.ecel;
         break;
-      case "EE":
+      case "Lighting Ears Ahead":
         eventList = widget.event.tech.ee;
         break;
     }
@@ -38,17 +52,81 @@ class _DepartmentsState extends State<Departments> {
             builder: (BuildContext context) => ListEvents(eventList)));
   }
 
+  void buildChildren() {
+    depts.forEach((f) {
+      children.add(
+        GestureDetector(
+          onTap: () {
+            go(f);
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(icons[depts.indexOf(f)]),
+                fit: BoxFit.cover,
+                colorFilter: new ColorFilter.mode(
+                    Colors.black12.withOpacity(0.7), BlendMode.darken),
+              ),
+            ),
+            child: Container(
+              width: double.infinity,
+              height: 200,
+              child: Center(
+                  child: Text(
+                f,
+                style: TextStyle(
+                  fontSize: 45,
+                  color: Colors.white,
+                  fontFamily: "sad films",
+                  shadows: <Shadow>[
+                    Shadow(
+                      offset: Offset(10.0, 10.0),
+                      blurRadius: 3.0,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                    Shadow(
+                      offset: Offset(10.0, 10.0),
+                      blurRadius: 8.0,
+                      color: Color.fromARGB(125, 0, 0, 0),
+                    ),
+                  ],
+                ),
+              )),
+            ),
+          ),
+        ),
+      );
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    buildChildren();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: depts.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(depts[index]),
-              onTap: () => go(index),
-            );
-          }),
-    );
+        body: SingleChildScrollView(
+      child: Container(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 0),
+          child: Column(
+            children: children,
+          ),
+        ),
+      ),
+    )
+//      ListView.builder(
+//          itemCount: depts.length,
+//          itemBuilder: (context, index) {
+//            return ListTile(
+//              title: Text(depts[index]),
+//              onTap: () => go(index),
+//            );
+//          }),
+        );
   }
 }
